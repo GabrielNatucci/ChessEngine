9397ef7d8c68c4c810e96f777a6b19ee90911af0
Gabriel Natucci <gabriel.natuccirusso@hotmail.com>
2025-06-13 19:45:37 -0300

    aff

--- run.sh
+++ run.sh
@@ -1,1 +1,6 @@
-mvn clean compile && mvn -q exec:java -Dexec.mainClass=com.natucciEngine.Main 
+# mvn clean compile && mvn -q exec:java -Dexec.mainClass=com.natucciEngine.Main 
+mvn clean compile && mvn -q exec:java \
+  -Dexec.mainClass=com.natucciEngine.Main \
+  -Dexec.args="" \
+  -Dexec.jvmArgs="-Xms512m -Xmx2g"
+
